33cc7d31f7a6660d6a8be29e2b051823d69c1f38
sq4567 <sq4567@gmail.com>
2024-01-15 12:04:11 +0900

    feat: Change

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from random import randint, choice
 
 
 def get_randint():
-    return [randint(1, 45) for _ in range(6)]
+    return [randint(1, 45) for _ in range(7)]
 
 
 def get_choice():
-    return [choice(range(1, 46)) for _ in range(6)]
+    return [choice(range(1, 46)) for _ in range(7)]
 
 if __name__ == '__main__':
     print(get_randint())
